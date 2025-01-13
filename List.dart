c82bdc2269944568340449b6c9134c9dcbb35957
Mohammad Asad Rahmani <146559979+rahmani3101@users.noreply.github.com>
2025-01-13 13:27:09 +0530

    Create List.dart

---
+++ List.dart
@@ -0,0 +1,17 @@
+void main() 
+{
+  // Create a list
+  List<int> numbers = [1, 2, 3, 4, 5];
+  
+  // Accessing elements
+  print(numbers[0]);  // Outputs: 1
+  
+  // Adding an element
+  numbers.add(6);
+  
+  // Iterating through the list
+  for (int number in numbers) 
+  {
+    print(number);
+  }
+}
