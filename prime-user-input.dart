05db85805c72997e725b29e217f291eaae4a9a0c
Mohammad Asad Rahmani <146559979+rahmani3101@users.noreply.github.com>
2025-01-14 19:32:29 +0530

    Create prime-user-input.dart

---
+++ prime-user-input.dart
@@ -0,0 +1,26 @@
+import 'dart:io';
+
+bool isPrime(int n) 
+{
+  if (n <= 1) 
+    return false;
+  for (int i = 2; i <= n ~/ 2; i++) 
+  {
+    if (n % i == 0) return false;
+  }
+  return true;
+}
+
+void main() 
+{
+  stdout.write("Enter a number: ");
+  int number = int.parse(stdin.readLineSync()!);
+  if (isPrime(number)) 
+  {
+    print('$number is a prime number');
+  } 
+  else 
+  {
+    print('$number is not a prime number');
+  }
+}
